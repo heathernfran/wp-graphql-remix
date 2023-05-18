b5bdbec815101bffc453e8853ccdb189de13c91f
heathernfran <heathernfran@gmail.com>
2023-05-17 19:47:29 -0700

    Add graphql to get project data from wordpress

---
+++ app/routes/projects._index.tsx
@@ -0,0 +1,8 @@
+export default function ProjectsIndexPage() {
+  return (
+    <>
+      <h2>Projects index route for wordpress data</h2>
+      <p>Projects were created using the Atlas Content Modeler plugin</p>
+    </>
+  );
+}

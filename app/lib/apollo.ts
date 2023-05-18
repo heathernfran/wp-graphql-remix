67e55dbd03cca87984cee15ab4727833154e0bb2
heathernfran <heathernfran@gmail.com>
2023-05-17 20:51:48 -0700

    Import default fetch

--- app/lib/apollo.ts
+++ app/lib/apollo.ts
@@ -1,4 +1,4 @@
-import { fetch } from "cross-fetch";
+import fetch from "cross-fetch";
 import { ApolloClient, InMemoryCache, createHttpLink } from "@apollo/client";
 
 const link = createHttpLink({

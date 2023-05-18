81d8f1881738a1de767398a5ee135617db16df1b
heathernfran <heathernfran@gmail.com>
2023-05-18 15:17:21 -0700

    Test link with ngrok

--- app/lib/apollo.ts
+++ app/lib/apollo.ts
@@ -2,7 +2,7 @@ import fetch from "cross-fetch";
 import { ApolloClient, InMemoryCache, createHttpLink } from "@apollo/client";
 
 const link = createHttpLink({
-  uri: "http://learn-wpgraphql.local/graphql",
+  uri: "https://48f7-71-197-175-167.ngrok-free.app/graphql",
   fetch,
 });
 

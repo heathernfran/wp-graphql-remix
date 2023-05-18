0a0579c87c2c885ac26cee2645afc7e8cb60c775
Heather Frantisak <heathernfran@gmail.com>
2023-05-17 18:09:40 -0700

    Projects slug (#1)
    * WIP Add project details page, query project by slug id
    
    * Fix missing default export in projects slug route

---
+++ app/routes/projects.$slug.tsx
@@ -0,0 +1,20 @@
+import { LoaderArgs, json } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
+import { getProjectsBySlug } from "~/lib/wordpressService";
+
+export async function loader({ params }: LoaderArgs) {
+  const projectBySlug = await getProjectsBySlug(params.slug);
+  return json({ projectBySlug });
+}
+
+export default function ProjectDetailsPage() {
+  const data = useLoaderData<typeof loader>();
+
+  return (
+    <div>
+      <h1>{data.projectBySlug.name}</h1>
+      <div>{data.projectBySlug.description}</div>
+      <p>{data.projectBySlug.dueDate}</p>
+    </div>
+  );
+}

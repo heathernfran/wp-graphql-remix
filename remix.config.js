a9030a5545214c06e666c76b3a60d72b49f2e067
heathernfran <heathernfran@gmail.com>
2023-05-17 19:23:30 -0700

    Update remix config for netlify

--- remix.config.js
+++ remix.config.js
@@ -1,6 +1,12 @@
 /** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
   cacheDirectory: "./node_modules/.cache/remix",
+  ignoredRouteFiles: ["**/.*"],
+  server:
+    process.env.NETLIFY || process.env.NETLIFY_LOCAL
+      ? "./server.ts"
+      : undefined,
+  serverBuildPath: ".netlify/functions-internal/server.js",
   future: {
     v2_errorBoundary: true,
     v2_meta: true,
